940a751eb7742de377e6949a64241b412ae52796
Daniel Luna <daniellunsc@gmail.com>
2018-06-03 20:02:03 -0300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import MainLogin from './Components/MainLogin'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <MainLogin />
+      </div>
+    );
+  }
+}
+
+export default App;
